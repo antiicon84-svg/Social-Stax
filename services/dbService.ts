e5f4c1961652ca64727caaa3ee922201061dcdca
antiicon84-svg <antiicon84@gmail.com>
2025-12-28 10:55:13 +0200

    fix: Repair corrupted imports in dbService.ts - fix from keyword and paths
    Signed-off-by: antiicon84-svg <antiicon84@gmail.com>

--- services/dbService.ts
+++ services/dbService.ts
@@ -13,9 +13,9 @@ import {
 } from "firebase/firestore";
 import { initializeApp, getApp } from "firebase/app";
 import { getFunctions, httpsCallable } from "firebase/functions";
-import { FIREBASE_CONFIG } fro../src/config/constants';
-import { Client, Post, UserProfile, FreeAccessGrant } from ../src/types';
-import { getCurrentUser } from "./authService";
+import { FIREBASE_CONFIG } from '../src/config/constants';
+import { Client, Post, UserProfile, FreeAccessGrant } from '../src/types'
+  ;import { getCurrentUser } from "./authService";
 
 const db = getFirestore(getApp());
 
