e877f72b292b72212388da537640a1f2f0074d2c
antiicon84-svg <antiicon84@gmail.com>
2025-12-28 11:06:57 +0200

    fix: Fix typo in WebRouter services import - ddbService to dbService
    Signed-off-by: antiicon84-svg <antiicon84@gmail.com>

--- src/routes/WebRouter.tsx
+++ src/routes/WebRouter.tsx
@@ -14,7 +14,7 @@ import PromptGuideView from '../../views/PromptGuideView';
 import BillingView from '../../views/BillingView';
 import LoadingSpinner from '../components/LoadingSpinner';
 import Button from '../components/Button';
-import { getClients, getScheduledPosts, deletePost } from '../../services/ddbService';
+import { getClients, getScheduledPosts, deletePost } from '../../services/dbService';
 import { Client, Post } from '../types';
 const ClientDetailWrapper: React.FC<{ onPostScheduled: () => void }> = ({ onPostScheduled }) => {
   const { clientId } = useParams<{ clientId: string }>();
